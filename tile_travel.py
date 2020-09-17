18dc20885f4cb95a7f3dc2ce5a4424216978d9f3
Robertleoj <robert.leo.jonsson@gmail.com>
2020-09-17 11:07:58 +0000

    Complete

--- tile_travel.py
+++ tile_travel.py
@@ -9,12 +9,12 @@ def check_directions(x_pos, y_pos):
     #For each direction, if movement is legal, add to output string
     out_str = ""
     if x_pos == 1:
-        if y_pos != 3:
-            out_str += "e"
-        if y_pos != 3:
+        if y_pos == 3:
+            out_str += "es"
+        if y_pos == 2:
+            out_str += "nes"
+        if y_pos == 1:
             out_str += "n"
-        if y_pos != 1:
-            out_str += "s"
             
     if x_pos == 2:
         if y_pos == 3:
@@ -26,7 +26,7 @@ def check_directions(x_pos, y_pos):
 
     if x_pos == 3:
         if y_pos == 3:
-            out_str += "ws"
+            out_str += "sw"
         if y_pos == 2:
             out_str += "ns"
         if y_pos == 1:
@@ -73,26 +73,34 @@ def print_options(legal_options):
         if ch == 'w':
             output_str += ' (W)est'
         multiple = True
-    return output_str
+    output_str += '.'
 
+    print(output_str)
     
 
     
 
+def game_loop():
+        
+    x_pos = 1
+    y_pos = 1
 
-x_pos = 1
-y_pos = 3
+    while not (x_pos == 3 and y_pos == 1):
 
-# while not (x_pos == 3 and y_pos == 1):
+        legal_options = check_directions(x_pos, y_pos)
+        
+        print_options(legal_options)
 
-    #Check directions save output,
-    #Print options
-    #Prompt for direction input
-    #Check if direction is valid (in string returned by check_directions())
-    #   if not valid, prompt for input again
+        direction = input('Direction: ')
+        direction = direction.lower()
 
-    #move_player()
-    #Repeat until Victory
+        if len(direction) ==1 and direction in legal_options:
+            x_pos, y_pos = move_player(x_pos, y_pos, direction)
 
-#else:
-#   Print victory message
+        else:
+            print('Not a valid direction!')
+
+    else:
+        print('Victory!')
+
+game_loop()
