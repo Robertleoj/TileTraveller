f284590eceea5a6f17321b7b95d73a95b6df73bd
Robertleoj <robert.leo.jonsson@gmail.com>
2020-09-17 09:36:03 +0000

    second Commit

---
+++ tile_travel.py
@@ -0,0 +1,42 @@
+
+def check_directions(x_pos, y_pos):
+
+    """
+    Argument: position of player
+
+    Returns: String of legal directions
+    """
+    #For each direction, if movement is legal, add to output string
+    pass
+
+
+def move_player(x_pos, y_pos, dir_str):
+
+    """
+    Argument: Position and direction to move
+
+    Returns: New position
+    """
+    #Add appropirate values to x_pos and y_pos and return them
+    pass
+
+def print_options(legal_options):
+    #Prints legal options for user
+
+
+x_pos = 1
+y_pos = 3
+
+# while not (x_pos == 3 and y_pos == 1):
+
+    #Check directions save output,
+    #Print options
+    #Prompt for direction input
+    #Check if direction is valid (in string returned by check_directions())
+    #   if not valid, prompt for input again
+
+    #move_player()
+    #Repeat until Victory
+
+#else:
+#   Print victory message
