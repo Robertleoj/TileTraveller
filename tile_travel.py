0e0c8020f6ec1f940269bcf504b81f6fc29fb716
JonBjSig <jaunanb@gmail.com>
2020-09-17 09:55:09 +0000

    Implementing algorithms

--- tile_travel.py
+++ tile_travel.py
@@ -7,7 +7,32 @@ def check_directions(x_pos, y_pos):
     Returns: String of legal directions
     """
     #For each direction, if movement is legal, add to output string
-    pass
+    out_str = ""
+    if x_pos == 1:
+        if y_pos != 3:
+            out_str += "e"
+        if y_pos != 3:
+            out_str += "n"
+        if y_pos != 1:
+            out_str += "s"
+            
+    if x_pos == 2:
+        if y_pos == 3:
+            out_str += "ew"
+        if y_pos == 2:
+            out_str += "sw"
+        if y_pos == 1:
+            out_str += "n"
+
+    if x_pos == 3:
+        if y_pos == 3:
+            out_str += "ws"
+        if y_pos == 2:
+            out_str += "ns"
+        if y_pos == 1:
+            out_str += "n"
+
+    return out_str
 
 
 def move_player(x_pos, y_pos, dir_str):
@@ -18,7 +43,15 @@ def move_player(x_pos, y_pos, dir_str):
     Returns: New position
     """
     #Add appropirate values to x_pos and y_pos and return them
-    pass
+    if dir_str == "n":
+        y_pos += 1
+    elif dir_str == "s":
+        y_pos -= 1
+    elif dir_str == "e":
+        x_pos += 1
+    else:
+        x_pos += 1
+    return x_pos, y_pos
 
 def print_options(legal_options):
     #Prints legal options for user
